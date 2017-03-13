31de22568a689402e7e3f4f98f082b6b4de3b7d9
Naomi Mine <naomi.mine@gmail.com>
2017-03-13 10:34:27 -0500

    first commit naomi

--- server/models/Coffee.js
+++ server/models/Coffee.js
@@ -1,1 +1,10 @@
 var mongoose = require('mongoose');
+
+var CoffeeSchema = new mongoose.Schema({
+
+
+})
+
+var coffeeModel = mongoose.model('Coffee', CoffeeSchema);
+
+module.exports = coffeeModel;
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,11 @@
 var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+	username: String
+
+
+})
+
+var userModel = mongoose.model('User', UserSchema);
+
+module.exports = userModel;
