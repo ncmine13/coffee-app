a154d4fc4bc957cebb226dab7fab8d3db5a17de6
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2017-03-15 15:52:16 -0500

    added some profile styling.. still working

--- server/app.js
+++ server/app.js
@@ -7,27 +7,45 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 
 require('./db/db');
-app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(session({
 	secret: " this is our secret salt",
 	resave: false,
 	saveUninitialized: true,
 	cookie: {secure: false}
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// var authenticateRoute = function(req, res, next){
+// 	if (req.originalUrl === '/user/login' || req.originalUrl === '/user/register') {
+// 		next();
+// 	}
+// 		else {
+// 			//there is not a loggedIn property , booleans
+// 			if (!req.session.loggedIn) {
+// 				res.redirect('/user/login')
+// 			}
+// 			else {
+// 				next();
+// 			}
+// 		}
+// 	}
+// //set before the controllers so it intercepts and checks before controllers start to take over (AKA middleware)
+// app.use(authenticateRoute);
 
 var UserController = require('./controllers/UserController');
 var DrinkController = require('./controllers/DrinkController');
 var ChoiceController = require('./controllers/ChoiceController')
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/coffee', DrinkController);
 app.use('/user', UserController);
 app.use('/path', ChoiceController);
-
-
 
 server.listen(3000, function(){
 	console.log("server is listening on 3000");
--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
 	username: String,
 	password: String,
+	image: String,
 	favoriteDrinks: [{type: mongoose.Schema.Types.ObjectId, ref:'Drink'}]
 })
 var userModel = mongoose.model('User', UserSchema);
