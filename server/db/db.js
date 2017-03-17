3b4633beeb3376fc7b8c1d34c8a739b943a64485
Naomi Mine <naomi.mine@gmail.com>
2017-03-17 10:50:16 -0500

    heroku

--- server/db/db.js
+++ server/db/db.js
@@ -7,7 +7,7 @@ mongoose.connection.on('connected', function(){
 	console.log('connected to ' + connectionString);
 })
 
-mongoose.connection.on('error', function(){
+mongoose.connection.on('error', function(error){
 	console.log('error ' + error);
 })
 
